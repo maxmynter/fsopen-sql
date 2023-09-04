765cebfa738041b9c6a0b9470d323b504a2c3851
Max Mynter <maxmynter@me.com>
2023-09-04 17:29:50 +0200

    Ex.13.6

--- blogs/controllers/blog.js
+++ blogs/controllers/blog.js
@@ -2,6 +2,11 @@ const router = require("express").Router()
 
 const { Blog } = require("../models")
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
 router.get("/", async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
@@ -20,6 +25,16 @@ router.delete("/:id", async (req, res) => {
   const id = req.params.id
   Blog.destroy({ where: { id } })
   return res.status(204).send()
+})
+
+router.put("/:id", blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog.likes)
+  } else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
