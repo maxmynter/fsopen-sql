2a343e8aca87096387c8ecec14911c2550bf6a6c
Max Mynter <maxmynter@me.com>
2023-09-04 12:11:12 +0200

    Ex. 13.3

--- commands.sql
+++ commands.sql
@@ -1,4 +1,4 @@
 Create table blogs (id integer primary key, author text, url text not null, title text not null, likes integer default 0);
-insert into blogs  values (1, 'Paul Graham', 'http://www.paulgraham.com/vb.html', 'Life is Short')
-insert into blogs values (2, 'Sam Altman', 'https://blog.samaltman.com/idea-generation', 'Idea Generation')
-SELECT + from blogs;
+insert into blogs  values (1, 'Paul Graham', 'http://www.paulgraham.com/vb.html', 'Life is Short');
+insert into blogs values (2, 'Sam Altman', 'https://blog.samaltman.com/idea-generation', 'Idea Generation');
+SELECT * from blogs;
