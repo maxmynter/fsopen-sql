4a7c0f96fe4cc7533ab4780b50dac0cbbf275006
Max Mynter <maxmynter@me.com>
2023-09-06 21:41:34 +0200

    Table Connection & Proper Note insertion

--- notes/controllers/notes.js
+++ notes/controllers/notes.js
@@ -7,14 +7,33 @@ const noteFinder = async (req, res, next) => {
   next()
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization")
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: "token invalid" })
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" })
+  }
+  next()
+}
+
 router.get("/", async (req, res) => {
   const notes = await Note.findAll()
   res.json(notes)
 })
 
-router.post("/", async (req, res) => {
+router.post("/", tokenExtractor, async (req, res) => {
   try {
-    const note = await Note.create(req.body)
+    const user = await User.findByPk(req.decodedToken.id)
+    const note = await Note.create({
+      ...req.body,
+      userId: user.id,
+      date: new Date(),
+    })
     res.json(note)
   } catch (error) {
     return res.status(400).json({ error })
